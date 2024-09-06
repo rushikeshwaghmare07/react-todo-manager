2c7240ea17bf059d972040eaabfc9f03cea215e9
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-09-06 15:25:24 +0530

    feat: implement add, update, delete, and toggle actions functionality

--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -10,7 +10,7 @@ export const TodoContext = createContext({
   ],
   addTodo: (todo) => {},
   updateTodo: (id, todo) => {},
-  delete: (id) => {},
+  deleteTodo: (id) => {},
   toggleComplete: (id) => {}
 });
 
